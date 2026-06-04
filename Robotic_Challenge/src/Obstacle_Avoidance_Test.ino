ec17167c740e1d4a0b507699c7fa11de72010a0c
Grisha_rubix <rubingd09@gmail.com>
2026-06-04 16:35:39 +0100

    Cleaned up directory for submission.

--- Robotic_Challenge/src/Obstacle_Avoidance_Test.ino
+++
@@ -1,2 +0,0 @@
-// #include "line-based/obstacle_Avoidance_line_based.h"
-#include "wall-based/obstacle_avoidance_wall_based.h"
--- Robotic_Challenge/test/6_04easy/secrets.example.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define WIFI_SSID "PhaseSpaceNetwork_2.4G"
-#define WIFI_PASSWORD "your_wifi_password_here"
-#define BROKER_HOST "192.168.0.74"
-#define BROKER_PORT 1883
-#define GROUP_ID "2"
